9151aa7e880a8e8e5a8d920502ee6badf339b6b8
alliaume.rico <alliaume.rico@gmail.com>
2015-04-13 22:42:27 +0200

    AJout du premier jet pour les modules

--- main/modules/external/get_content.php
+++ main/modules/external/get_content.php
@@ -37,11 +37,50 @@ if((isset($_GET['page']))&&(!empty($_GET['page']))) {
 
 
     ob_start();
-    include $GLOBALS['BASE_PATH'].'main/scripts/'.$page.'.php';
-    include $GLOBALS['BASE_PATH'].'main/scripts/post_script.php';
-    include $GLOBALS['BASE_PATH'].'main/libs/js/page_'.$page.'.js';
-    include $GLOBALS['BASE_PATH'].'main/templates/'.$page.'.html';
-    include $GLOBALS['BASE_PATH'].'main/libs/js/send_info_error.js';
+    
+    // Check if it's a plugin or not
+    if (in_array($page, $GLOBALS['PLUGIN']))
+    {
+        // This is a plugin !
+        // Load lib
+        $fileName = $GLOBALS['BASE_PATH'] . 'main/plugin/' . $page . '/lib_' . $page . '.php';
+        if (is_file($fileName)) 
+        {
+            require_once $fileName;
+        }
+    
+        // Load script
+        $fileName = $GLOBALS['BASE_PATH'] . 'main/plugin/' . $page . '/script_' . $page . '.php';
+        if (is_file($fileName)) {
+            include $fileName;
+        }
+        include $GLOBALS['BASE_PATH'].'main/scripts/post_script.php';
+        
+        // Load javascript
+        $fileName = $GLOBALS['BASE_PATH'] . 'main/plugin/' . $page . '/' . $page . '.js';
+        if (is_file($fileName)) {
+            include $fileName;
+        }
+        
+        // Load HTML
+        $fileName = $GLOBALS['BASE_PATH'] . 'main/plugin/' . $page . '/' . $page . '.html';
+        if (is_file($fileName)) {
+            include $fileName;
+        }
+        
+        include $GLOBALS['BASE_PATH'].'main/libs/js/send_info_error.js';
+        
+    }
+    else
+    {
+        // This is not a plugin :-(
+        include $GLOBALS['BASE_PATH'].'main/scripts/'.$page.'.php';
+        include $GLOBALS['BASE_PATH'].'main/scripts/post_script.php';
+        include $GLOBALS['BASE_PATH'].'main/libs/js/page_'.$page.'.js';
+        include $GLOBALS['BASE_PATH'].'main/templates/'.$page.'.html';
+        include $GLOBALS['BASE_PATH'].'main/libs/js/send_info_error.js';
+    }
+
     $include = ob_get_clean();
     echo $include;
 }
