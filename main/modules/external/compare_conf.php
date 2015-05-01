63b7d143713b74ff38386e67ad7fbfcf00ed0e4f
ygene <yann.genevois@gmail.com>
2015-05-01 12:05:30 +0200

    Amélioration du diff

--- main/modules/external/compare_conf.php
+++ main/modules/external/compare_conf.php
@@ -8,7 +8,7 @@
     $tmp_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'];
     $current_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi";
 
-    exec("diff -r $tmp_conf $current_conf",$ret,$err);
+    exec("diff -rw $tmp_conf $current_conf",$ret,$err);
 
     echo json_encode($err);
 ?>
--- main/modules/external/diff_conf.php
+++ main/modules/external/diff_conf.php
@@ -1,8 +1,12 @@
 <?php
 
+require_once('../../libs/config.php');
+$tmp_conf       = $GLOBALS['CULTIPI_CONF_TEMP_PATH'];
+$current_conf   = $GLOBALS['CULTIPI_CONF_PATH'] . "/01_defaultConf_RPi";
 $diff="";
-if(is_dir("/etc/cultipi/conf_tmp")&&is_dir("/etc/cultipi/01_defaultConf_RPi")) {
-    $diff=shell_exec("diff -r /etc/cultipi/01_defaultConf_RPi /etc/cultipi/conf_tmp");
+
+if(is_dir("$tmp_conf")&&is_dir("$current_conf")) {
+    $diff=shell_exec("diff -ruw $current_conf $tmp_conf");
 }
 
 echo nl2br($diff);
