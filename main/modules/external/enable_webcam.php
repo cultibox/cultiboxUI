988d0345a4b9fea225393e001cbe490f35cf440b
ygene <yann.genevois@gmail.com>
2015-06-12 10:00:23 +0200

    Correction enable culticam

--- main/modules/external/enable_webcam.php
+++ main/modules/external/enable_webcam.php
@@ -3,5 +3,6 @@
 $action=$_GET['action'];
 $webcam=$_GET['webcam'];
 exec("echo $webcam > /var/lock/culticam_$action",$output,$err); 
-sleep(10);
+exec("sudo chown cultipi var/lock/culticam_$action",$output,$err);
+sleep(5);
 ?>
