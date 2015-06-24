ecac54605e8bc64bc143d6a8fac61e99eb1c9365
ygene <yann.genevois@gmail.com>
2015-06-24 20:12:04 +0200

    Optimisation snapshot webcam

--- main/modules/external/enable_webcam.php
+++ main/modules/external/enable_webcam.php
@@ -2,13 +2,24 @@
 
 $action=$_GET['action'];
 $webcam=$_GET['webcam'];
-exec("echo $webcam > /tmp/culticam_$action",$output,$err); 
+exec("echo $webcam > /tmp/culticam_$action",$output,$err);
 exec("sudo chown cultipi:cultipi /tmp/culticam_$action",$output,$err);
 exec("sudo mv /tmp/culticam_$action /var/lock/",$output,$err);
 if(strcmp("$action","stream")==0) {
     sleep(10);
 } else {
-    sleep(5);
+    if(is_file("/var/www/cultibox/tmp/webcam$webcam.jpg")) {
+        exec("sudo mv /var/www/cultibox/tmp/webcam$webcam.jpg /tmp/",$output,$err);
+    }
+    $count=0;
+    while(true) {
+        sleep(1);
+        $count=$count+1;
+        if((is_file("/var/www/cultibox/tmp/webcam$webcam.jpg"))||($count>10)) {
+            break;
+        }
+    }
+    sleep(2);
 }
 
 ?>
