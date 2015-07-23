cd71b5fb36bad96741636af92ebf99df6baa9350
Yann Genevois <yann.genevois@cultibox.fr>
2015-07-23 17:42:18 +0200

    Gestion du deuxième utilisateur

--- main/modules/external/reset_password.php
+++ main/modules/external/reset_password.php
@@ -5,6 +5,10 @@ if((isset($_GET['pwd']))&&(!empty($_GET['pwd']))) {
    exec("(echo -n 'cultipi:Identification:' && echo -n 'cultipi:Identification:".$_GET['pwd']."' | md5sum | awk '{print $1}') >> /tmp/.passwd",$output,$err);
    if($err==0) {
        if(is_file("/tmp/.passwd")) {
+           exec("sudo cat /etc/lighttpd/.passwd|grep root",$output,$err);
+           if((count($output)==1)&&(strcmp($output[0],"")!=0)) {
+               exec("echo $output[0] >> /tmp/.passwd",$output,$err);
+           }
            exec("sudo mv /tmp/.passwd /etc/lighttpd/",$output,$err);
        } else {
          $err=1;
