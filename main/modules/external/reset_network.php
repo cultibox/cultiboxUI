8d64b0c204e08dc1cee99f2424f8f433c699b951
Yann Genevois <yann.genevois@cultibox.fr>
2015-11-01 16:50:35 +0100

    utilisation de hub-ctrl

--- main/modules/external/reset_network.php
+++ main/modules/external/reset_network.php
@@ -1,35 +1,9 @@
 <?php
 
 exec("sudo /bin/cp /etc/network/interfaces.BASE /etc/network/interfaces",$output,$err);
-exec("/sbin/lsmod 2>/dev/null|/bin/grep \"ath9k\"",$output,$err);
-exec("/sbin/lsmod 2>/dev/null|/bin/grep \"ath9k\"",$output,$err);
-if(count($output)>0) {
-    //For Atheros drivers :
-    exec("sudo /sbin/modprobe -r ath9k_htc",$output,$err);
-    exec("sudo /sbin/modprobe -r ath9k_common",$output,$err);
-    exec("sudo /sbin/modprobe -r ath9k_hw",$output,$err);
-    exec("sudo /sbin/modprobe -r ath",$output,$err);
-
-    exec("sleep 2",$output,$err);
-
-    exec("sudo /sbin/modprobe ath9k_htc",$output,$err);
-    exec("sudo /sbin/modprobe ath9k_common",$output,$err);
-    exec("sudo /sbin/modprobe ath9k_hw",$output,$err);
-    exec("sudo /sbin/modprobe ath",$output,$err);
-} else {
-    //For driver TP-Link TL-WN725N  and others :
-    exec("/sbin/lsmod 2>/dev/null|/bin/grep \"8192cu\"",$output,$err);
-    if(count($output)>0) {
-        exec("sudo /sbin/modprobe -r 8192cu",$output,$err);
-        exec("sleep 2",$output,$err);
-        exec("sudo /sbin/modprobe 8192cu",$output,$err);
-    } else {
-        //For Wipi driver:
-        exec("sudo /sbin/modprobe -r rt2800usb",$output,$err);
-        exec("sleep 2",$output,$err);
-        exec("sudo /sbin/modprobe rt2800usb",$output,$err);
-     }
-}
+exec("sudo /usr/sbin/hub-ctrl -h 0 -P 2 -p 0",$output,$err);
+sleep(3);
+exec("sudo /usr/sbin/hub-ctrl  -h 0 -P 2 -p 1",$output,$err);
 sleep(7);
 exec("sudo /usr/sbin/invoke-rc.d networking force-reload",$output,$err);
 sleep(2);
